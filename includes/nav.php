5ab1c91a90aafaef21901cbdac1468812556740e
msevani <msevani@gmail.com>
2016-12-01 16:48:09 +0000

    NAV: Fixed facebook link

--- includes/nav.php
+++ includes/nav.php
@@ -1,6 +1,6 @@
 	<nav class="navbar navbar-default">
 	  <div class="container-fluid">
-	    <div class="navbar-header">
+	    <div class="navbar-header">	   
 	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
@@ -16,10 +16,9 @@
 	        <li <?php print $activeApply;?>><a href="/apply/">Apply</a></li>
 	        <li <?php print $activeFAQ;?>><a href="/faqs/">FAQs</a></li>
 	      </ul>
-
-	      <ul class="nav navbar-nav navbar-right">
+	      <ul class="nav navbar-nav navbar-right hidden-xs">
 			<li><a href="mailto:yfs@sewauk.org"><i class="fa fa-envelope" aria-hidden="true"></i>yfs@sewauk.org</a></li>
-			<li class="fa fa-facebook"><a href="https://www.facebook.com/youth4sewa/" target="_blank"></a></li>
+			<a href="https://www.facebook.com/youth4sewa/" target="_blank"><li class="fa fa-facebook"></li></a>
 	      </ul>
 	    </div>
 	  </div>
