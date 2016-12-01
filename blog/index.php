d316f96d743602e40b3a049c10efc725b39768ca
msevani <msevani@gmail.com>
2016-12-01 15:49:49 +0000

    Updated blog

--- blog/index.php
+++ blog/index.php
@@ -5,6 +5,7 @@
 		<div class="row">
 			<div class="col col-sm-12">
 				<h2><span class="brand">YFS</span> Intern Blogs</h2>
+				<p>Here are a selection of blogs written by our recent interns.</p>
 			</div>
 		</div>
 		<div class="row">
@@ -12,20 +13,36 @@
 				<h4>Hem Mistry</h4>
 				<h3>YFS – Why & Initial Thoughts</h3>
 				<p class="date">Posted on August 3, 2016</p>
+				<img src="images/img_2.jpg" class="img-thumbnail right">
 				<p>Sewa in its simplest form is Selfless Service and to me it is greater than charity work through the relationships / personal touch made with those that you are serving. Due to my background as a Swayamsevak, the act of Sewa is promoted and is of great focus within our work, and any opportunity, no matter how small or large, should be taken if you are able to.</p>
 				<a href="https://hemmistry.wordpress.com" target="_blank">Read Hem's blog</a>
 				<hr />
+				
 				<h4>Suraj Tailor</h4>
 				<h3>Longings, Development and Thoughts</h3>
-				<strong>Posted on July 17, 2016</strong>
+				<p class="date">Posted on July 17, 2016</p>
 				<p>Three weeks from now I will be making my second trip to India as part of the YFS initiative. YFS (Youth for Sewa) is an internship program which takes on those of age 18-25. Then sends them to work on various sewa projects around India. The one I will be working on, is in the outskirts of Mumbai working for a charity which seeks to stop young children, who run away from home, being exploited by dangerous people. This blog essentially highlights my reasons for embarking on YFS and my general thoughts before going.</p>
 				<a href="https://surajtailor.wordpress.com" target="_blank">Read Suraj's blog</a>
 				<hr />
+				
 				<h4>Pavni Lakhani</h4>
 				<h3>A bike, a box and a boy</h3>
-				<strong>Posted on October 22, 2015</strong>
-				<p>When I reached the tekro this morning I stood at a gate, shouting for Ramesh bhai, the man who delivers the tiffin boxes. I had asked to accompany him to see what he did. I shouted and waited. I heard some shuffling coming from the room above and then slowly I heard the light tread of someone moving down the stairs. The gate opened and I smiled. He stood there, legs in the air, arms firmly on the floor, smiling at me and welcoming me into his home.</p>
+				<p class="date">Posted on October 22, 2015</p>
+				<img src="images/img_1.jpg" class="img-thumbnail left">
+				<p>When I reached the tekro this morning I stood at a gate, shouting for Ramesh bhai, the man who delivers the tiffin boxes. I had asked to accompany him to see what he did. I shouted and waited. I heard some shuffling coming from the room above and then slowly I heard the light tread of someone moving down the stairs. The gate opened and I smiled. He stood there, legs in the air, arms firmly on the floor, smiling at me and welcoming me into his home.</p>				
 				<a href="https://smileforchangeblog.wordpress.com/" target="_blank">Read Pavni's blog</a>				
+				<hr />
+
+				<h4>Bansri Lakhani</h4>
+				<h3>Being Indian: Education, Dedication and Choice</h3>
+				<p class="date">Posted on October 22, 2015</p>
+				<p>Taking a career break and wanting to discover India, I decided to spend four months working for several NGO’s in Northern Indian. I share here three encounters, which consequently challenged and changed my perspective and helped me redefine who I am today.</p>
+				<p>Perfectly co-ordinated in the most beautiful, traditional, quirky punjabi suit, she walks in into the room filled with men and women of all ages and backgrounds. She’s intelligent, kind and softly spoken with an air of confidence and humility all at once. She leads a Government initiative program for the education and nourishment of pre-school children in the slums of Ahmedabad. We watch the sunset from her veranda, sip chai and talk about being an unmarried, educated woman, living in India. As we move onto politics and social injustice, I ask her if she has ever thought about moving, she replies, <blockquote>When your mother grows old and frail and looses her beauty, you don’t abandon her or find another one. In the same way, how can you say that you will leave India if she is your mother and that there is no hope?</blockquote></p>
+				<p>Mad about food, with a Masters degree in Sanskrit, she teaches Niyudh (martial arts) and patiently shows me how to run the colored sand through my fingers to make a Rangoli. We dart through the lush streets of Pune on her scooter, admiring the scaled down models of Maharashtra’s forts, she narrates stories of Shivaji Maharaj to me, as I hold on tight. We talk about her family’s disapproval of her choice to dedicate her life to work full time for India’s largest grassroots organisation for women. “Mara-Sangor phool hote hai, after blossoming they naturally fall to the ground, ye swadarpan hai.” Using the flowers as an example of dedication she goes on, “if you want to dedicate your life to service or Bharat Mata do so only once you have fully blossomed, developed and grown.”</p>
+				<p>Living in Ahmedabad’s largest slum area, sitting on the rugged floor with her elderly husband she waits patiently for us to deliver her daily meal. When we arrive, she quickly empties the food, washes the tiffin and returns it to me. I watch as she divides the portion barely enough for one, into three. Surprised I ask why? She smiles slowly and in her smile I see years of hardship, love and experience all at once. I am stunned by her beauty and her reply, “It is ok if we don’t eat but the dogs outside our home should not go hungry”</p>
+				<p>For the first time I felt what it meant to be Indian, I experienced the beauty of our rich heritage, I connected with strangers, dined with children and danced with Ma-jis (elderly mothers).</p>
+				<p>As my love for India grew, I saw the country, the people and the culture from a new perspective. My sense of identity evolved as I slowly started to understand those around me and felt responsible for the challenges India faces.</p>
+				<p>I learnt what it means to truly serve.</p>
 			</div>
 		</div>
 	</section>
