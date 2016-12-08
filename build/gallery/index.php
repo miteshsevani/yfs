bb6ab48592e08607815bb6369172d284768e34d2
msevani <msevani@gmail.com>
2016-12-08 16:41:39 +0000

    Gallery: Built new 'slick'  gallery

--- build/gallery/index.php
+++ build/gallery/index.php
@@ -1,222 +1,120 @@
 <?php include('../includes/header.php'); ?>
 <?php include('../includes/nav.php'); ?>
-<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.css">
+  
+<style type="text/css">
+
+.slider { width: 100%; background: #eee; padding-top:25px;}
+.slick-slide { margin: 0px 20px; text-align: center;}
+.slick-prev:before,
+.slick-next:before { color: black; }
+
+@media (max-width: 767px) { 
+    .slick-dots,button { display:none !important; } 
+}
+
+.slick-slide::-moz-selection { color: red}
+.slick-slide::selection { color: red; }
+</style>
+
 <section class="main page-gallery">
-	<div class="container-fluid">
-    <div id="custom_carousel" class="carousel slide" data-ride="carousel" data-interval="4000">
-        <!-- Wrapper for slides -->
-        <div class="carousel-inner">
-            <div class="item active">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-1.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Learning, reading, engaging</p>
-                        </div>
-                    </div>
-                </div>           
+	<div class="container-fluid">    
+        <section class="single-item slider">
+            <div>
+              <img src="/gallery/images/gi-1.jpg" />
+              <p class="caption">Learning, reading, engaging</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-2.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Hem and Suraj attending board meetings to procure funding for Samatol</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <img src="/gallery/images/gi-2.jpg" />
+                <p class="caption">Hem and Suraj attending board meetings to procure funding for Samatol</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-3.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>There's always time for cricket after a busy day at the railway station</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <img src="/gallery/images/gi-3.jpg" />
+                <p class="caption">There's always time for cricket after a busy day at the railway station</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-4.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>A glimpse of Mumbai's railway stations. Due to high security photos are prohibited</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <img src="/gallery/images/gi-4.jpg" />
+                <p class="caption">A glimpse of Mumbai's railway stations. Due to high security photos are prohibited</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-5.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Pitching the work that Samatol do at a funding meeting</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <img src="/gallery/images/gi-5.jpg" />
+                <p class="caption">Pitching the work that Samatol do at a funding meeting</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-6.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Monkey - ing around with the Children at Samatol</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <img src="/gallery/images/gi-6.jpg" />
+                <p class="caption">Monkey - ing around with the Children at Samatol</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-7.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Suraj playing with the Runaway children</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <img src="/gallery/images/gi-7.jpg" />
+                <p class="caption">Suraj playing with the Runaway children</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-8.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Busy planting, making the centre a greener place for the children</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <img src="/gallery/images/gi-8.jpg" />
+                <p class="caption">Busy planting, making the centre a greener place for the children</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-9.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>More games and fun with the children</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <img src="/gallery/images/gi-9.jpg" />
+                <p class="caption">More games and fun with the children</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-10.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Celebrating Rakshabandan</p>
-                        </div>
-                    </div>
-                </div>
+            <div>            
+                <img src="/gallery/images/gi-10.jpg" />
+                <p class="caption">Celebrating Rakshabandan</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-11.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Celebrating Rakshabandan</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+                <img src="/gallery/images/gi-11.jpg" />
+                <p class="caption">Celebrating Rakshabandan</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-12.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Celebrating Rakshabandan</p>
-                        </div>
-                    </div>
-                </div>
+            <div>       
+                <img src="/gallery/images/gi-12.jpg" />
+                <p class="caption">Celebrating Rakshabandan</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-13.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Rakshabandan antics with the children at Samatol</p>
-                        </div>
-                    </div>
-                </div>
+            <div>       
+                <img src="/gallery/images/gi-13.jpg" />
+                <p class="caption">Rakshabandan antics with the children at Samatol</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-14.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Our expert gardeners  at work</p>
-                        </div>
-                    </div>
-                </div>
+            <div>       
+                <img src="/gallery/images/gi-14.jpg" />
+                <p class="caption">Our expert gardeners  at work</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-15.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Our expert gardeners  at work</p>
-                        </div>
-                    </div>
-                </div>
+            <div>       
+                <img src="/gallery/images/gi-15.jpg" />
+                <p class="caption">Our expert gardeners  at work</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-16.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>Children at the Kamalini Karnabadhir Vidyalaya</p>
-                        </div>
-                    </div>
-                </div>           
+            <div>       
+                <img src="/gallery/images/gi-16.jpg" />
+                <p class="caption">Children at the Kamalini Karnabadhir Vidyalaya</p>
             </div>
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-17.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>A snapshot of the tribal communities we work with at the Banswara  project</p>
-                        </div>
-                    </div>
-                </div>            
-            </div>             
-            <div class="item">
-                <div class="container-fluid">
-                    <div class="row">
-                        <div class="top col-xs-12"><img src="/gallery/images/gi-18.jpg" class="img-responsive"></div>
-                        <div class="content col-xs-12">
-                            <p>All ready for the Kamalini  karnabadhir vidyalaya Diwali  performance</p>
-                        </div>
-                    </div>
-                </div>           
+            <div>
+                <img src="/gallery/images/gi-17.jpg" />
+                <p class="caption">A snapshot of the tribal communities we work with at the Banswara  project</p>
             </div>
-        <!-- End Item -->
-        </div>
-        <a data-slide="prev" href="#custom_carousel" class="izq carousel-control">‹</a>
-        <a data-slide="next" href="#custom_carousel" class="der carousel-control">›</a>
-        <!-- End Carousel Inner -->
-        <div class="controls draggable ui-widget-content col-xs-12">
-            <ul class="nav ui-widget-header">
-                <li data-target="#custom_carousel" data-slide-to="0" class="active"><a href="#"><img src="/gallery/images/gi-1.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="1"><a href="#"><img src="/gallery/images/gi-2.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="2"><a href="#"><img src="/gallery/images/gi-3.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="3"><a href="#"><img src="/gallery/images/gi-4.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="4"><a href="#"><img src="/gallery/images/gi-5.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="5"><a href="#"><img src="/gallery/images/gi-6.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="6"><a href="#"><img src="/gallery/images/gi-7.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="7"><a href="#"><img src="/gallery/images/gi-8.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="8"><a href="#"><img src="/gallery/images/gi-9.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="9"><a href="#"><img src="/gallery/images/gi-10.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="10"><a href="#"><img src="/gallery/images/gi-11.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="11"><a href="#"><img src="/gallery/images/gi-12.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="12"><a href="#"><img src="/gallery/images/gi-13.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="13"><a href="#"><img src="/gallery/images/gi-14.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="14"><a href="#"><img src="/gallery/images/gi-15.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="15"><a href="#"><img src="/gallery/images/gi-16.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="16"><a href="#"><img src="/gallery/images/gi-17.jpg"></a></li>
-                <li data-target="#custom_carousel" data-slide-to="17"><a href="#"><img src="/gallery/images/gi-18.jpg"></a></li>
-            </ul>
-        </div>
+            <div>                
+                <img src="/gallery/images/gi-18.jpg" />
+                <p class="caption">All ready for the Kamalini  karnabadhir vidyalaya Diwali  performance</p>
+            </div>
+        </section>
     </div>
-    <!-- End Carousel -->
-</div>
 </section>
 
+
+<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.js" type="text/javascript" charset="utf-8"></script>
+  <script type="text/javascript">
+    $(document).on('ready', function() {
+      $('.single-item').slick({
+        dots: true,
+        arrows: true,
+      });
+      $(".variable").slick({
+        dots: true,
+        infinite: true,
+        variableWidth: true
+      });
+    });
+  </script>
+
+
+
 <?php include('../includes/footer.php'); ?>
-<script src="/js/min/gallery-min.js"></script>
