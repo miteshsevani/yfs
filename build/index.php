dfc9f63931be38b0382abbb005fd2e8ea02e8971
Mitesh Sevani <miteshsevani@Mitesh-Sevani.local>
2017-02-13 11:05:05 +0000

    Restyled home page and added new content

--- build/index.php
+++ build/index.php
@@ -12,7 +12,7 @@
 
 	<section class="main page-home">
 		<div class="row">
-			<div class="col col-sm-6">
+			<div class="col col-sm-7">
 				<h2>What is <span class="brand">YFS</span>?</h2>				
  				<p>The internship focuses on self-development through the journey of service in underprivileged communities. It is an opportunity for individuals to contribute their energy, time and ideas and make a real difference.</p>
  				<p>The program is designed to actualize the meaning of Sewa – Selfless Service.</p>
@@ -20,15 +20,14 @@
 				<p>Interns spend the duration of their internship working with NGOs in the community.</p>
 				<p>Through service, they will discover, learn and engage in a transformative experience that empowers them, as they strive to make an impact in their chosen field.</p>
 			</div>		
-			<div class="col col-sm-6">
+			<div class="col col-sm-5">
 				<h2>Impact</h2>
-				<div class='videoWrapper'><iframe width='100%' height='280' src='https://www.youtube.com/embed/mQQazX1deBU?rel=0' frameborder='0' allowfullscreen></iframe></div>
+				<div class='videoWrapper'><iframe width='100%' height='220' src='https://www.youtube.com/embed/mQQazX1deBU?rel=0' frameborder='0' allowfullscreen></iframe></div>
 				
 				<h3 style="text-transform: none;margin-top:20px;">Stay connected</h3>
 				<iframe style="float:left;margin-right:10px;" src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Fyouth4sewa%2F&width=90&layout=button_count&action=like&size=small&show_faces=false&share=false&height=65&appId=193840110715213" width="75" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
 
 				<iframe style="float:left;"" src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fsewauk.org%2Fyfs%2F&layout=button_count&size=small&mobile_iframe=true&appId=193840110715213&width=90&height=28" width="75" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
-
 			</div>
 		</div>
 	</section>	
